139a17e564f12a80781f2e9154c3de0fac6c3687
aathish20 <130310215+aathish20@users.noreply.github.com>
2023-04-10 19:18:10 +0530

    Update login.php

--- php/login.php
+++ php/login.php
@@ -3,10 +3,10 @@
 
     if (isset($_POST['login'])) 
 	{
-		$connection = new mysqli('localhost', 'root', '', 'login1');
+		$connection = new mysqli('localhost', 'root', '', 'login');
 		
 		$email = $connection->real_escape_string($_POST['emailPHP']); 
-		$password = $connection->real_escape_string($_POST['passwordPHP']); 
+		$password = md5($connection->real_escape_string($_POST['passwordPHP'])); 
 
 		$data = $connection->query("select id from users where email='$email' AND password='$password'");
 		if($data->num_rows > 0)
